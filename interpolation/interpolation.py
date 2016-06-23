8bcf88c1138f83d36a6f4b471f7ff357d3b8c4b1
carlegroen <carlegroen@gmail.com>
2016-06-23 14:38:19 +0200

    added function

--- interpolation/interpolation.py
+++ interpolation/interpolation.py
@@ -1,6 +1,18 @@
 from __future__ import division
 import numpy as np
-import search
+
+#
+# BINARY SEARCH
+#
+
+def bin(x,z):
+	low=0
+	high=len(x)-1
+	while high-low>1:
+		mid=int(round((high+low)/2))
+		if z >= x[mid]:	low=mid
+		else: high=mid
+	return low
 
 #
 # POLYNOMIAL INTERPOLATION
