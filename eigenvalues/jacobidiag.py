bf2ae0fc4721129d2d9e3412bce867e0d16941be
carlegroen <carlegroen@gmail.com>
2016-06-26 16:16:03 +0200

    easiest so far

--- eigenvalues/jacobidiag.py
+++ eigenvalues/jacobidiag.py
@@ -2,9 +2,10 @@ from __future__ import division
 import math
 import numpy as np
 
-def jacobi(A):
 
-    n = np.shape(A)[0]
+
+def jacobidiag(A):
+        n = np.shape(A)[0]
 	d = np.zeros(n)
 	for i in range(n):
 		d[i]=A[i,i]
@@ -17,3 +18,42 @@ def jacobi(A):
             		for j in range(i+1,n):
                 		rotated, sweeps=rotate(i,j,n,A,V,d,rotated,sweeps)
     	return d,V,sweeps
+
+
+def rotate(p,q,n,A,V,d,rotated,sweeps):
+	app = d[p]
+	aqq = d[q]
+	apq = A[p,q]
+	phi = 0.5*np.arctan2(2.0*apq,aqq-app)
+	c = np.cos(phi)
+	s = np.sin(phi)
+	App_new = c*c*app+s*s*aqq-2.0*s*c*apq
+	Aqq_new = s*s*app+c*c*aqq+2.0*s*c*apq
+	if (App_new != app) or (Aqq_new != aqq):
+        	rotated = True
+        	sweeps += 1
+		d[p] = App_new
+		d[q] = Aqq_new
+		A[p,q] = 0.0
+		for i in range(p):
+			aip = A[i,p]
+			aiq = A[i,q]
+			A[i,p] = c*aip-s*aiq
+			A[i,q] = c*aiq+s*aip
+		for i in range(p+1,q):
+			api = A[p,i]
+			aiq = A[i,q]
+			A[p,i] = c*api-s*aiq
+			A[i,q] = c*aiq+s*api
+		for i in range(q+1,n):
+			api = A[p,i]
+			aqi = A[q,i]
+			A[p,i] = c*api-s*aqi
+			A[q,i] = c*aqi+s*api
+		for i in range(n):
+			vip = V[i,p]
+			viq = V[i,q]
+			V[i,p] = c*vip-s*viq
+			V[i,q] = c*viq+s*vip
+
+	return rotated, sweeps
--- eigenvalues/main.py
+++ eigenvalues/main.py
@@ -1,3 +1,79 @@
 from __future__ import division
-from jacobi import *
+from jacobidiag import *
 import numpy as np
+import sys
+from numpy import linalg as LA
+
+A    = np.array([[1.0,6.0,3.0],[6.0,4.0,-5.0],[3.0,-5.0,6.0]])
+Aorg = np.copy(A)
+print "Un-changed matrix A:"
+print A
+
+EigenvalA,EigenvecA,sweeps = jacobidiag(A)
+npEigenval,npEigenvec = LA.eig(Aorg)
+print "Eigenvalues of A:"
+print EigenvalA
+print "Eigenvectors of A:"
+print EigenvecA
+print "Number of sweeps:"
+print sweeps
+print "----------------------------------"
+print "Eigenvalues and eigenvectors found with numpy.linalg:"
+print "Eigenvalues:"
+print npEigenval
+print "Eigenvectors:"
+print npEigenvec
+print "----------------------------------"
+print "Orthogonality of the eigenvectors:"
+V1=EigenvecA[:,0]
+V2=EigenvecA[:,1]
+V3=EigenvecA[:,2]
+print "v1 * v2"
+check1=0
+n=len(V1)
+for i in range(n):
+	check1+=V1[i]*V2[i]
+print check1
+print "v1 * v3"
+check2=0
+for i in range(n):
+	check2+=V1[i]*V3[i]
+print check2
+print "v2 * v3"
+check3=0
+for i in range(n):
+	check3+=V2[i]*V3[i]
+print check3
+print "----------------------------------"
+print "Check normalization"
+check1=0
+print "v1"
+for i in range(n):
+	check1+=V1[i]*V1[i]
+print np.sqrt(check1)
+check2=0
+print "v2"
+for i in range(n):
+	check2+=V2[i]*V2[i]
+print np.sqrt(check2)
+check3=0
+print "v3"
+for i in range(n):
+	check3+=V3[i]*V3[i]
+print np.sqrt(check3)
+print "Checking eigenvectors:"
+print "A * v1 = lamb1 * v1"
+print "A * v1"
+print np.dot(Aorg,V1)
+print "lamb1 * v1"
+print EigenvalA[0]*V1
+print "A * v2 = lamb2 * v2"
+print "A * v2"
+print np.dot(Aorg,V2)
+print "lamb2 * v2"
+print EigenvalA[1]*V2
+print "A * v3 = lamb3 * v3"
+print "A * v3"
+print np.dot(Aorg,V3)
+print "lamb3 * v3"
+print EigenvalA[2]*V3
