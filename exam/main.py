ebe66622c6d59db5c919fd77359011813a656aaa
carlegroen <carlegroen@gmail.com>
2016-06-28 18:18:31 +0200

    bilinear interpolation complete hombres

--- exam/main.py
+++ exam/main.py
@@ -17,7 +17,7 @@ j = 0
 
 for i in range(len(x)):
     for j in range(len(y)):
-        F[j][i] = 2*x[i]*y[j]
+        F[j][i] = x[i]*y[j]
 
 Flist = np.ravel(F)
 
@@ -33,23 +33,23 @@ for i in range(len(Flist)):
     n[i][2] = Flist[i]
 
 kappa = np.zeros((100,100))
-pxx = np.zeros(600)
-pyy = np.zeros(600)
 
 count = 0
-px = 0.01
-
+px = 0
+countx = 0
 while px < 1:
+    county = 0
     count = 1
-    py = 0.01
+    py = 0
     while py < 1:
         #print count
         count += 1
-        kappa[px*100,py*100] = bilin.bilinear(x, y, F, px, py)
+        kappa[countx,county] = bilin.bilinear(x, y, F, px, py)
+        county += 1
         py += 0.01
     px += 0.01
+    countx +=1
 
-print kappa
 # def column(matrix, i):
 #     return [row[i] for row in matrix]
 #
@@ -63,7 +63,7 @@ plt.colorbar()
 plt.savefig('noninterpolated.png',format='png')
 
 plt.figure(2)
-plt.contourf(np.linspace(0,1,100),np.linspace(0,1,100),kappa,100)
+plt.contourf(np.linspace(0,1,100),np.linspace(0,1,100),kappa,len(kappa))
 plt.savefig('interpolated.png',format='png')
 
 
