88975d3e6db2d22e29ffd4e723cab0ae581313ed
carlegroen <carlegroen@gmail.com>
2016-06-28 18:36:44 +0200

    finished interpolated thingy. missing makefile

--- exam/main.py
+++ exam/main.py
@@ -6,7 +6,6 @@ np.seterr(divide='ignore', invalid='ignore')
 
 x = np.array([0,1])
 y = np.array([0,1])
-z = np.linspace(x[0],len(x),100)
 nx = len(x)
 ny = len(y)
 F = np.zeros((nx,ny))
@@ -19,18 +18,12 @@ for i in range(len(x)):
     for j in range(len(y)):
         F[j][i] = x[i]*y[j]
 
-Flist = np.ravel(F)
-
 k = 0
 l = 0
 for k in range(len(x)):
     n[0+len(x)*l:len(x)+len(x)*l,0] = x[l]
     n[0+len(x)*l:len(x)+len(x)*l,1] = y[l]
     l += 1
-
-i = 0
-for i in range(len(Flist)):
-    n[i][2] = Flist[i]
 
 kappa = np.zeros((100,100))
 
@@ -50,13 +43,6 @@ while px < 1:
     px += 0.01
     countx +=1
 
-# def column(matrix, i):
-#     return [row[i] for row in matrix]
-#
-# xvals = column(n,0)
-# yvals = column(n,1)
-# Fvals = column(n,2)
-
 plt.figure(1)
 plt.contourf(x,y,F)
 plt.colorbar()
@@ -64,22 +50,5 @@ plt.savefig('noninterpolated.png',format='png')
 
 plt.figure(2)
 plt.contourf(np.linspace(0,1,100),np.linspace(0,1,100),kappa,len(kappa))
+plt.colorbar()
 plt.savefig('interpolated.png',format='png')
-
-
-#
-#
-#
-#
-#
-# plt.imshow(F)
-# plt.savefig('test.png',format='png')
-#
-# print 'Original Array:'
-# print [x,y]
-#
-# print 'F array'
-# print F
-#
-# print 'Bilinear interpolation'
-# print f1
