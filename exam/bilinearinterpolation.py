511a457fe29b4d1804fc969073f1e60cc0df2f3c
carlegroen <carlegroen@gmail.com>
2016-06-29 11:40:23 +0200

    fixed part B

--- exam/bilinearinterpolation.py
+++ exam/bilinearinterpolation.py
@@ -8,7 +8,7 @@ def bilinear(x,y,F,px,py):
     j = search.bin(y,py)
     a1 = F[i][j]
     a2 = F[i + 1][j] - F[i][j]
-    a3 = F[i][j+i] - F[i][j]
+    a3 = F[i][j + 1] - F[i][j]
     a4 = F[i][j] - F[i + 1][j] - F[i][j + 1] + F[i + 1][j + 1]
     p = a1 + a2 * (px - x[i]) + a3 * (py - y[j]) + a4 * (px - x[i]) * (py - y[j])
     return p
--- exam/mainB.py
+++ exam/mainB.py
@@ -3,55 +3,71 @@ import numpy as np
 import bilinearinterpolation as bilin
 np.seterr(divide='ignore', invalid='ignore')
 
-x = np.array([0,1,3])
-y = np.array([0,1,3])
+x = np.array([0,1,2,3])
+y = np.array([0,1,2,3])
 nx = len(x)
 ny = len(y)
 F = np.zeros((nx,ny))
 n = np.zeros((nx*ny,3))
+steplength = 0.02
+kappa = np.zeros((max(x)/steplength,max(y)/steplength))
 
 i = 0
 j = 0
 
+#Define F matrix given a function. Here, it is sqrt(x^2+y^2)
+
 for i in range(len(x)):
     for j in range(len(y)):
-        F[j][i] = np.sin(x[i]*x[i]+y[j]*y[j])#x[j]*x[j]-y[i]*y[i]
+        F[j][i] = np.sqrt(x[i]*x[i]+y[j]*y[j])
 
 print F
 
-k = 0
-l = 0
-for k in range(len(x)):
-    n[0+len(x)*l:len(x)+len(x)*l,0] = x[l]
-    n[0+len(x)*l:len(x)+len(x)*l,1] = y[l]
-    l += 1
+#Defining F after a matrix with known looks
+F[0][0] = 1.0
+F[1][0] = 3.0
+F[2][0] = 6.0
+F[3][0] = 4.0
+F[0][1] = 1.0
+F[1][1] = 2.0
+F[2][1] = 3.0
+F[3][1] = 5.0
+F[0][2] = 2.0
+F[1][2] = 4.0
+F[2][2] = 3.0
+F[3][2] = 1.0
+F[0][3] = 5.0
+F[1][3] = 3.0
+F[2][3] = 2.0
+F[3][3] = 3.0
 
-kappa = np.zeros((150,150))
 
+#Starts performing the bilinear interpolation in correct sequence
 count = 0
 px = 0
 countx = 0
-while px < 3:
+while px < max(x):
     county = 0
     count = 1
     py = 0
-    while py < 3:
+    while py < max(y):
         #print count
         count += 1
         kappa[countx,county] = bilin.bilinear(x, y, F, px, py)
         county += 1
-        py += 0.02
-    px += 0.02
+        py += steplength
+    px += steplength
     countx +=1
 
 
-
+#Plots non-interpolated figure
 plt.figure(1)
 plt.contourf(x,y,F)
 plt.colorbar()
 plt.savefig('noninterpolatedPartB.png',format='png')
 
+#Plots interpolated figure
 plt.figure(2)
-plt.contourf(np.linspace(0,3,countx),np.linspace(0,3,county),kappa,len(kappa))
+plt.contourf(np.linspace(min(x),max(x),countx),np.linspace(min(y),max(y),county),kappa,len(kappa))
 plt.colorbar()
 plt.savefig('interpolatedPartB.png',format='png')
